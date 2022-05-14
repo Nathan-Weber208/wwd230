da56fbab67f439849fc7da990f6a3b59b1bc3469
Nathan-Weber <nathanwbr@gmail.com>
2022-05-14 12:52:22 -0600

    Hamberger navigation

---
+++ chamber/js/dates.js
@@ -0,0 +1,34 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let d = new Date(document.lastModified);
+console.log(d)
+let dayname = daynames[d.getDay()];
+let monthname = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayname + ", " + monthname + " " + d.getDate() + ", " + year;
+
+
+document.getElementById("currentdate").innerHTML = fulldate;
+document.getElementById("currentdate2").innerHTML = fulldate;
+document.getElementById("currentyear").innerHTML = year;
