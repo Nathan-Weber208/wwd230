7848f22ba3b92ed147b8eef80a618e83b7311bac
Nathan-Weber <nathanwbr@gmail.com>
2022-05-28 22:00:18 -0600

    windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -4,7 +4,7 @@
 // let ip2 = document.getElementById('inputtwo').value;
 // let s = parseFloat(ip2);
 let s = 20
-let t = 80
+let t = 60
   
-let f = (0.0817*(3.71*(Math.pow(s, 0.5))+5.81-0.25*s)*(t-91.4)+91.4);
-document.getElementById('message').innerHTML =f;  
+let f = Math.round(0.0817*(3.71*(Math.pow(s, 0.5))+5.81-0.25*s)*(t-91.4)+91.4);
+document.getElementById('Windchill').innerHTML =f;  
